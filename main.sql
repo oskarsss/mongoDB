42639d28c2bf4dd9df81936028f04fc8ea61be4f
oskarsss <89988568+oskarsss@users.noreply.github.com>
2022-01-03 18:02:14 +0200

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,23 @@
+db.sutdenti.insertMany([
+   {
+      vards: 'Oskars',
+      uzvards: 'Pucka',
+      dzimums: 'v',
+      valodas: [ 'Latviešu', 'Angļu', 'Ķīniešu', 'Krievu' ],
+      dzimsanas_diena: ISODate("2000-08-16T00:00:00.000Z")
+   },
+   {
+      vards: 'Aivis',
+      uzvards: 'Kuzmenoks',
+      dzimums: 'v',
+      valodas:[ 'Latviešu', 'Angļu', 'Krievu' ],
+      dzimsanas_diena: ISODate("2000-12-19T00:00:00.000Z")
+   }
+   {
+      vards: 'Laura',
+      uzvards: 'Tuska',
+      dzimums: 's',
+      valodas: [ 'Latviešu', 'Angļu', 'Franču', 'Krievu' ],
+      dzimsanas_diena: ISODate("2000-02-13T00:00:00.000Z")
+   }
+])
